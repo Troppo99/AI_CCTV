de7a9e17ffd47e5e7f640c93343f122d10570873
Troppo99 <troppolungo99@gmail.com>
2024-04-29 13:18:28 +0700

    update yolo basic

--- Learn02/yoloBasic.py
+++ Learn02/yoloBasic.py
@@ -31,11 +31,26 @@ import cv2
 import cvzone
 import math
 
-cap = cv2.VideoCapture(1)
-cap.set(3,1080)
-cap.set(4,720)
+# cap = cv2.VideoCapture(0)
+# cap.set(3,1080)
+# cap.set(4,720)
+# cap = cv2.VideoCapture('project1(AI CCTV)\Data set\Folding Area_proses sortir.mp4')
+cap = cv2.VideoCapture('Learn02/Videos/1_ch04_20240418061050.mp4')
 
-model = YOLO('Learn02/Yolo-Weights/yolov8n.pt')
+
+model = YOLO('Learn02/Yolo-Weights/yolov8l.pt')
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
 while True:
     succes, img = cap.read()
@@ -50,13 +65,15 @@ while True:
             
             w,h = x2-x1,y2-y1
             cvzone.cornerRect(img, (x1,y1,w,h))
-
+            # confidence
             conf = math.ceil((box.conf[0]*100))/100
-            print(conf)
-            cvzone.putTextRect(img,f'{conf}',(max(0,x1),max(y1-20)))
+            # Class Name
+            cls = int(box.cls[0])
+
+            cvzone.putTextRect(img,f'{classNames[cls]} {conf}',(max(0,x1),max(35,y1)),scale = 1.5,thickness=2,colorT=(25,50,100),colorR=(0,255,255))
 
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
-cap.release()
+cap.release()     
 cv2.destroyAllWindows()
