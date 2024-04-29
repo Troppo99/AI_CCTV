d99f4e83070ee4a607c4b78214e2a0647245aa02
Troppo99 <troppolungo99@gmail.com>
2024-04-29 09:34:54 +0700

    update

--- Learn02/yoloBasic.py
+++ Learn02/yoloBasic.py
@@ -29,23 +29,31 @@
 from ultralytics import YOLO
 import cv2
 import cvzone
+import math
 
 cap = cv2.VideoCapture(1)
-cap.set(3,1366)
-cap.set(4,768)
+cap.set(3,1080)
+cap.set(4,720)
 
 model = YOLO('Learn02/Yolo-Weights/yolov8n.pt')
 
 while True:
     succes, img = cap.read()
-    result = model(img, stream=True)
-    for r in result:
+    results = model(img, stream=True)
+    for r in results:
         boxes = r.boxes
         for box in boxes:
             x1,y1,x2,y2 = box.xyxy[0]
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
-            print(x1,y1,x2,y2)
-            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+            # cv2.reangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+            # print(x1,y1,x2,y2)
+            
+            w,h = x2-x1,y2-y1
+            cvzone.cornerRect(img, (x1,y1,w,h))
+
+            conf = math.ceil((box.conf[0]*100))/100
+            print(conf)
+            cvzone.putTextRect(img,f'{conf}',(max(0,x1),max(y1-20)))
 
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xff == ord('q'):
