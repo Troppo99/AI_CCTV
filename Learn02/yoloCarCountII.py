dc77117cc5be3e305da40882ed70971f49c717b5
Troppo99 <troppolungo99@gmail.com>
2024-05-14 14:01:17 +0700

    u

--- Learn02/yoloCarCountII.py
+++ Learn02/yoloCarCountII.py
@@ -94,10 +94,16 @@ classNames = [
     "hair drier",
     "toothbrush",
 ]
+
+# masking
 mask = cv2.imread("Learn02/Images/mask2.png")
 
 # Tracking
 tracker = sort.Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+# limits = [423, 297, 673, 297]
+limits = [399, 297, 640, 297]
+totalCount = []
 
 # looping
 while True:
@@ -113,9 +119,7 @@ while True:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             w, h = x2 - x1, y2 - y1
-            # confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
-            # Class Name
             cls = int(box.cls[0])
             currentClass = classNames[cls]
 
@@ -126,23 +130,47 @@ while True:
                 or currentClass == "motorbike"
                 and conf > 0.5
             ):
-                cvzone.putTextRect(
-                    img,
-                    f"{currentClass} {conf}",
-                    (max(0, x1), max(35, y1)),
-                    scale=1.5,
-                    thickness=2,
-                    colorT=(25, 50, 100),
-                    colorR=(0, 255, 255),
-                )
-                cvzone.cornerRect(img, (x1, y1, w, h), l=9, t=3)
+                # cvzone.putTextRect(
+                #     img,
+                #     f"{currentClass} {conf}",
+                #     (max(0, x1), max(35, y1)),
+                #     scale=1.5,
+                #     thickness=2,
+                #     colorT=(25, 50, 100),
+                #     colorR=(0, 255, 255),
+                # )
+                # cvzone.cornerRect(img, (x1, y1, w, h), l=9, t=3, rt=5)
                 currentArray = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections, currentArray))
 
     resultTracker = tracker.update(detections)
+    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
+
     for result in resultTracker:
-        x1, y1, x2, y2, Id = result
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         print(result)
+
+        w, h = x2 - x1, y2 - y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, t=3, rt=5, colorR=(255, 0, 0))
+        cvzone.putTextRect(
+            img,
+            f"ID: {int(id)}",
+            (max(0, x1), max(35, y1)),
+            scale=1.5,
+            thickness=2,
+            offset=3,
+        )
+
+        cx, cy = x1 + w // 2, y1 + h // 2
+        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        if limits[0] < cx < limits[2] and limits[1] - 20 < cy < limits[3] + 20:
+            if totalCount.count(id) == 0:
+                totalCount.append(id)
+
+    cvzone.putTextRect(img, f"Count: {totalCount}", (50, 50))
+
     img = cv2.resize(img, newDim)
     cv2.imshow("Image", img)
     # cv2.imshow("Image Region", imgRegion)
