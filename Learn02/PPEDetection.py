e7c55208bfec20b5621fac38c9c853094b77014e
Troppo99 <troppolungo99@gmail.com>
2024-05-15 13:55:58 +0700

    u

--- Learn02/PPEDetection.py
+++ Learn02/PPEDetection.py
@@ -32,7 +32,7 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
             w, h = x2 - x1, y2 - y1
-            #             cvzone.cornerRect(img, (x1, y1, w, h))
+            # cvzone.cornerRect(img, (x1, y1, w, h))
             # confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
             # Class Name
