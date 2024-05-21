af779678f42061df4819441dd60c39718e69e5b1
Troppo99 <troppolungo99@gmail.com>
2024-05-21 08:41:40 +0700

    update

--- Project01/src/bismillah_sec.py
+++ Project01/src/bismillah_sec.py
@@ -1,7 +1,10 @@
+# Include library
 from ultralytics import YOLO
 import cv2
 import cvzone
 import math
+import time
+from datetime import timedelta
 
 cap = cv2.VideoCapture(0)
 model = YOLO("../MY_FILES/Yolo-Models/ppe.pt")
@@ -17,13 +20,20 @@ classNames = [
     "machinery",
     "vehicle",
 ]
-myColor = (128,128,128)
+
+# Colors settings
+myColor = (128, 128, 128)
 bulao = (255, 0, 0)
 hejo = (0, 255, 0)
 bereum = (0, 0, 255)
-koneng = (0,255,255)
-hideung = (0,0,0)
-bodas = (255,255,255)
+koneng = (0, 255, 255)
+hideung = (0, 0, 0)
+bodas = (255, 255, 255)
+
+# Time settings
+mask_wearing = False
+mask_start_time = 0
+mask_total_duration = 0
 
 while True:
     succes, img = cap.read()
@@ -40,27 +50,25 @@ while True:
             currentClass = classNames[cls]
 
             """LET'S PLAY WITH YOUR BRAIN"""
-            read = 0
-            if conf > 0.5:
-                if currentClass == "NO-Mask":
-                    myColor = bereum
-                    read = 1
-                elif currentClass == "Mask":
-                    myColor = hejo
-                    read = 1
-                if read == 1:
-                    cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 2)
-                    cvzone.putTextRect(
-                        img,
-                        f"{classNames[cls]} {conf}",
-                        (max(0, x1), max(0, y2+15)),
-                        scale=1,
-                        thickness=1,
-                        colorT=koneng,
-                        colorR=hideung,
-                        colorB=myColor,
-                        offset=1,
-                    )
+            colors = {
+                "NO-Mask": bereum,  # Assuming 'bereum' is previously defined as some BGR color
+                "Mask": hejo,  # Assuming 'hejo' is another BGR color
+            }
+
+            if conf > 0.5 and currentClass in ["NO-Mask", "Mask"]:
+                myColor = colors[currentClass]
+                cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 2)
+                cvzone.putTextRect(
+                    img,
+                    f"{classNames[cls]} {conf}",
+                    (max(0, x1), max(0, y2 + 15)),
+                    scale=1,
+                    thickness=1,
+                    colorT=koneng,  # Assuming 'koneng' is defined as a color
+                    colorR=hideung,  # Assuming 'hideung' is defined as another color
+                    colorB=myColor,
+                    offset=1,
+                )
             """LET'S PLAY WITH YOUR BRAIN"""
 
     cv2.imshow("Image", img)
