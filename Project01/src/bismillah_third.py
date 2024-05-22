24cfd2aaa06334e18ae79a3ebdf1da17ad0047ef
Troppo99 <troppolungo99.gmail.com>
2024-05-22 16:05:24 +0700

    bismillah_third still undone ID is not fix

--- Project01/src/bismillah_third.py
+++ Project01/src/bismillah_third.py
@@ -1,8 +1,25 @@
+"""
+Program Title: AI CCTV
+Author: Nana Wartana (alias Troppo Lungo)
+Field: Robotic and Automation
+Company: Gistex Garmen Indonesia
+Copyright (c) May 2024 Nana Wartana. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the above copyright notice, this list of conditions, 
+and the following disclaimer are retained.
+
+THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY; without even the implied 
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+"""
+
 from ultralytics import YOLO
 import cv2
 import cvzone
 import math
 import time
+import sort
+import numpy as np
 
 # Initialize video capture
 cap = cv2.VideoCapture("../MY_FILES/Videos/CCTV/Train/10_ch04_20240425073845.mp4")
@@ -28,11 +45,14 @@ frame_delay = int(1000 / original_fps)  # Delay in milliseconds
 
 # Initialize VideoWriter object with the original frame rate
 out = cv2.VideoWriter(
-    "output_video.avi",
+    "runs/videos/output_video.avi",
     cv2.VideoWriter_fourcc(*"XVID"),
     original_fps,
     (frame_width, frame_height),
 )
+
+# Initialize the SORT tracker
+tracker = sort.Sort(max_age=30, min_hits=3, iou_threshold=0.3)
 
 while True:
     start_time = time.time()
@@ -42,16 +62,18 @@ while True:
 
     # Detect people using the first model
     results_person = model_person(img, stream=True)
+    detections = np.empty((0, 5))  # Initialize detections array
 
     for r in results_person:
         boxes = r.boxes
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2 - x1, y2 - y1
             conf = math.ceil((box.conf[0] * 100)) / 100
             cls = int(box.cls[0])
             if conf > 0.25 and cls == 0:  # Class 0 is usually 'person' in COCO dataset
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
                 cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
                 cvzone.putTextRect(
                     img,
@@ -65,6 +87,24 @@ while True:
                     offset=5,
                 )
 
+    # Update tracker with detections
+    resultTracker = tracker.update(detections)
+
+    for result in resultTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (100, 220, 20), 3)
+        cvzone.putTextRect(
+            img,
+            f"ID: {int(id)}",
+            (max(0, x1), max(35, y1)),
+            scale=2,
+            thickness=2,
+            colorT=(0, 0, 255),
+            colorR=(0, 255, 0),
+            offset=5,
+        )
+
     # Detect custom classes using the second model
     results_custom = model_custom(img, stream=True)
 
@@ -73,23 +113,22 @@ while True:
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2 - x1, y2 - y1
             conf = math.ceil((box.conf[0] * 100)) / 100
             cls = int(box.cls[0])
             currentClass = classNames[cls]
-            if conf > 0.25:
-                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                cvzone.putTextRect(
-                    img,
-                    f"{currentClass} {conf}",
-                    (max(0, x1), max(35, y1)),
-                    scale=2,
-                    thickness=2,
-                    colorT=(0, 0, 255),
-                    colorR=(0, 255, 255),
-                    colorB=(0, 252, 0),
-                    offset=5,
-                )
+            # if conf > 0.25:
+            #     cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            #     cvzone.putTextRect(
+            #         img,
+            #         f"{currentClass} {conf}",
+            #         (max(0, x1), max(35, y1)),
+            #         scale=2,
+            #         thickness=2,
+            #         colorT=(0, 0, 255),
+            #         colorR=(0, 255, 255),
+            #         colorB=(0, 252, 0),
+            #         offset=5,
+            #     )
 
     # Write the frame to the video file
     out.write(img)
