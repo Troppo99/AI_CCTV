5a7cda5612131a85708e46bf00bc259f642f1f12
Troppo99 <troppolungo99.gmail.com>
2024-05-24 15:09:44 +0700

    updating

--- Project01/src/bimsillah_fourth.py
+++ Project01/src/bimsillah_fourth.py
@@ -4,69 +4,115 @@ import cvzone
 import math
 import time
 
-# Initialize video capture
-cap = cv2.VideoCapture("../MY_FILES/Videos/CCTV/Train/10_ch04_20240425073845.mp4")
 
-# Initialize YOLO model
-model = YOLO("runs/detect/train_employees/weights/best.pt")
+def initialize_video_capture(video_path):
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Error: Unable to open video file.")
+    return cap
 
-# Class names
-classNames = [
-    "Neneng",
-    "Imas",
-    "Euis",
-    "Siti",
-    "Enok",
-    "Puti",
-    "Sausan",
-    "Eti",
-    "Atik",
-    "Imam",
-]
 
-# Dimensions for imshow
-scaleof = 0.75  # 0 to 1.5 (1280 x 720 default video resolution)
-newDim = (int(1280 * scaleof), int(720 * scaleof))
+def initialize_video_writer(output_path, frame_width, frame_height, fps):
+    return cv2.VideoWriter(
+        output_path, cv2.VideoWriter_fourcc(*"XVID"), fps, (frame_width, frame_height)
+    )
 
-# Get frame width, height, and original frame rate
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-original_fps = cap.get(cv2.CAP_PROP_FPS)
-frame_delay = int(1000 / original_fps)  # Delay in milliseconds
 
-# Initialize VideoWriter object with the original frame rate
-out = cv2.VideoWriter(
-    "runs/videos/output_video.avi",
-    cv2.VideoWriter_fourcc(*"XVID"),
-    original_fps,
-    (frame_width, frame_height),
-)
-
-while True:
-    start_time = time.time()
-    success, img = cap.read()
-    if not success:
-        print("...computer vision is complete or fail...")
-        break
-
-    results = model(img, stream=True)
-
+def process_detections(results, img, class_names, confidence_threshold):
+    detections = []
     for r in results:
         boxes = r.boxes
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             w, h = x2 - x1, y2 - y1
-            # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
-            # Class Name
             cls = int(box.cls[0])
-            currentClass = classNames[cls]
-            if conf > 0.8:
+            currentClass = class_names[cls]
+            if conf > confidence_threshold:
+                detections.append((x1, y1, x2, y2, currentClass, conf))
+    return detections
+
+
+def main(
+    video_path, output_path, model_people_path, model_activities_path, scale_factor=0.75
+):
+    cap = initialize_video_capture(video_path)
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    original_fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_delay = int(1000 / original_fps)
+    new_dim = (int(frame_width * scale_factor), int(frame_height * scale_factor))
+
+    out = initialize_video_writer(output_path, frame_width, frame_height, original_fps)
+
+    model_people = YOLO(model_people_path)
+    model_activities = YOLO(model_activities_path)
+
+    class_names_people = [
+        "Neneng",
+        "Imas",
+        "Euis",
+        "Siti",
+        "Enok",
+        "Puti",
+        "Sausan",
+        "Eti",
+        "Atik",
+        "Imam",
+    ]
+    class_names_activities = ["Wrapping", "unloading", "packing", "sorting"]
+
+    while True:
+        start_time = time.time()
+        success, img = cap.read()
+        if not success:
+            print("...computer vision is complete or fail...")
+            break
+
+        results_people = model_people(img, stream=True)
+        results_activities = model_activities(img, stream=True)
+
+        detections_people = process_detections(
+            results_people, img, class_names_people, 0.8
+        )
+        detections_activities = process_detections(
+            results_activities, img, class_names_activities, 0.25
+        )
+
+        # Combine detections and display them
+        for x1, y1, x2, y2, person_class, person_conf in detections_people:
+            activity_detected = False
+            for (
+                ax1,
+                ay1,
+                ax2,
+                ay2,
+                activity_class,
+                activity_conf,
+            ) in detections_activities:
+                if (x1 <= ax1 <= x2 and y1 <= ay1 <= y2) or (
+                    x1 <= ax2 <= x2 and y1 <= ay2 <= y2
+                ):
+                    activity_detected = True
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                    cvzone.putTextRect(
+                        img,
+                        f"{person_class} {person_conf}, {activity_class} {activity_conf}",
+                        (max(0, x1), max(35, y1)),
+                        scale=2,
+                        thickness=2,
+                        colorT=(0, 0, 255),
+                        colorR=(0, 255, 255),
+                        colorB=(0, 252, 0),
+                        offset=5,
+                    )
+                    break
+            if not activity_detected:
                 cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
                 cvzone.putTextRect(
                     img,
-                    f"{classNames[cls]} {conf}",
+                    f"{person_class} {person_conf}",
                     (max(0, x1), max(35, y1)),
                     scale=2,
                     thickness=2,
@@ -76,22 +122,25 @@ while True:
                     offset=5,
                 )
 
-    # Write the frame to the video file
-    out.write(img)
+        out.write(img)
 
-    # Display the frame
-    img_resized = cv2.resize(img, newDim)
-    cv2.imshow("Image", img_resized)
+        # Resize the frame before displaying
+        img_resized = cv2.resize(img, new_dim)
+        cv2.imshow("Image", img_resized)
 
-    # Calculate processing time and add delay
-    processing_time = time.time() - start_time
-    wait_time = max(
-        1, frame_delay - int(processing_time * 4000)
-    )  # Ensure non-negative wait time
-    if cv2.waitKey(wait_time) & 0xFF == ord("q"):
-        break
+        processing_time = time.time() - start_time
+        wait_time = max(1, frame_delay - int(processing_time * 1000))
+        if cv2.waitKey(wait_time) & 0xFF == ord("q"):
+            break
 
-# Release the VideoCapture and VideoWriter objects
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    video_path = "../MY_FILES/Videos/CCTV/Train/10_ch04_20240425073845.mp4"
+    output_path = "runs/videos/output_video.avi"
+    model_people_path = "runs/detect/train_employees/weights/best.pt"
+    model_activities_path = "runs/detect/train_subhanallah/weights/best.pt"
+    main(video_path, output_path, model_people_path, model_activities_path)
