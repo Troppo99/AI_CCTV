4fe13a123d5d5361b9e4796843e898a8d38a622b
Troppo99 <troppolungo99.gmail.com>
2024-05-31 12:58:38 +0700

    train pending

--- train/Employees-3_C.py
+++ train/Employees-3_C.py
@@ -2,14 +2,15 @@ from ultralytics import YOLO
 
 
 def main():
-    data_path = "D:/NWR27/MY_FILES/Datasets/Employees-3/data.yaml"
-    model = YOLO(".runs/weights/yolov8l.pt")
+    data_path = "../MY_FILES/Datasets/Employees-3/data.yaml"
+    model = YOLO(".runs/detect/Employees-3/weights/last.pt")
     model.train(
         data=data_path,
         epochs=50,
         imgsz=640,
         project=".runs/detect",
-        name="Employees-3",
+        name="Employees-3_C",
+        resume=True,
     )
 
 
