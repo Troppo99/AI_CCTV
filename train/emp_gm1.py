60d3c7767cecf6918bf100e923ca56fb91c4cbe8
Troppo99 <troppolungo99.gmail.com>
2024-06-10 16:26:07 +0700

    MAU TRAIN

--- train/emp_gm1.py
+++ train/emp_gm1.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO(".runs/weights/yolov8l.pt")
-model.train(data="", epochs=50, imgsz=640, project=".runs/detect", name="emp_gm1")
+model.train(data="", epochs=50, imgsz=640, project=".runs/detect", name="")
