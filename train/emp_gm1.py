7ddb0ca5c59a18dd52d661937ef3510e13b81e53
Troppo99 <troppolungo99.gmail.com>
2024-06-12 07:52:43 +0700

    buat resume

--- train/emp_gm1.py
+++ train/emp_gm1.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     model = YOLO(".runs/weights/yolov8x.pt")
-    model.train(data="D:/NWR27/MY_FILES/Datasets/emp_gm1/data.yaml", epochs=50, imgsz=640, project=".runs/detect", name="emp_gm1")
+    model.train(data="D:/NWR27/MY_FILES/Datasets/emp_gm1/data.yaml", epochs=50, imgsz=640, project=".runs/detect", name="emp_gm1", resume=True)
