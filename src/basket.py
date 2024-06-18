86517a4b0a4a5da88b8e247cac20d191c2f1a10c
Troppo99 <troppolungo99.gmail.com>
2024-06-18 07:33:04 +0700

    stuck

--- src/basket.py
+++ src/basket.py
@@ -4,59 +4,56 @@ import cvzone
 import math
 import time
 
-# cap = cv2.VideoCapture("D:/NWR27/AI_CCTV/.runs/videos/record1_e.mp4")
-cap = cv2.VideoCapture("D:/NWR27/AI_CCTV/.runs/videos/record1_e.mp4")
-model = YOLO(".runs/detect/basket/weights/best.pt")
-classNames = ["0", "2", "3", "basket"]
-scaleof = 0.75
-newDim = (int(1280 * scaleof), int(720 * scaleof))
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-original_fps = cap.get(cv2.CAP_PROP_FPS)
-frame_delay = int(1000/ original_fps)
-out = cv2.VideoWriter(
-    ".runs/videos/test.avi",
-    cv2.VideoWriter_fourcc(*"XVID"),
-    original_fps,
-    (frame_width, frame_height),
-)
 
-while True:
-    start_time = time.time()
-    success, img = cap.read()
-    if not success:
-        break
-    results = model(img, stream=True)
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2 - x1, y2 - y1
-            conf = math.ceil((box.conf[0] * 100)) / 100
-            cls = int(box.cls[0])
-            currentClass = classNames[cls]
-            if conf > 0.3:
-                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                cvzone.putTextRect(
-                    img,
-                    f"{classNames[cls]} {conf}",
-                    (max(0, x1), max(35, y1)),
-                    scale=2,
-                    thickness=2,
-                    colorT=(0, 0, 255),
-                    colorR=(0, 255, 255),
-                    colorB=(0, 252, 0),
-                    offset=5,
-                )
-    out.write(img)
-    img_resized = cv2.resize(img, newDim)
-    cv2.imshow("Image", img_resized)
-    processing_time = time.time() - start_time
-    wait_time = max(1, frame_delay - int(processing_time * 1000))
-    if cv2.waitKey(wait_time) & 0xFF == ord("n"):
-        break
+def video_writer(cap):
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    original_fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_delay = int(1000 / original_fps)
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    return frame_width, frame_height, original_fps, frame_delay
+
+
+def main(video_path, model_path, scale):
+    cap = cv2.VideoCapture(video_path)
+    model = YOLO(model_path)
+    classNames = ["0", "2", "3", "basket"]
+    newDim = (int(1280 * scale), int(720 * scale))
+    frame_width, frame_height, original_fps, frame_delay = video_writer(cap)
+    out = cv2.VideoWriter(".runs/videos/test.avi", cv2.VideoWriter_fourcc(*"XVID"), original_fps, (frame_width, frame_height))
+
+    while True:
+        start_time = time.time()
+        success, img = cap.read()
+        if not success:
+            break
+        results = model(img, stream=True)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                cls = int(box.cls[0])
+                currentClass = classNames[cls]
+                if conf > 0:
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                    cvzone.putTextRect(img, f"{currentClass} {conf}", (max(0, x1), max(35, y1)), scale=2, thickness=2, colorT=(0, 0, 255), colorR=(0, 255, 255), colorB=(0, 252, 0), offset=5)
+        out.write(img)
+        img_resized = cv2.resize(img, newDim)
+        cv2.imshow("Image", img_resized)
+        processing_time = time.time() - start_time
+        wait_time = max(1, frame_delay - int(processing_time * 1000))
+        if cv2.waitKey(wait_time) & 0xFF == ord("n"):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    video_path = "D:/NWR27/AI_CCTV/.runs/videos/record1_e.mp4"
+    model_path = ".runs/detect/basket/weights/best.pt"
+    scale = 0.75
+    main(video_path, model_path, scale)
