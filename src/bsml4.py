0b2db8da95a8a87511a7cacaea99e7e4d6d51700
Troppo99 <troppolungo99@gmail.com>
2024-06-21 16:22:24 +0700

    CP1: Persiapan ROI

--- src/bsml4.py
+++ src/bsml4.py
@@ -6,19 +6,13 @@ import cvzone
 
 def main(video_path, scale):
     cap = cv2.VideoCapture(video_path)
-    model = YOLO(".runs/weights/act.pt")
+    model = YOLO(".runs/weights/yolov8l.pt")
     pink = (255, 0, 255)
     pink_reduce = (255, 200, 255)
 
-    # Read and resize mask once outside the loop
-    mask = cv2.imread(".runs/images/mask4.png")
-    _, frame = cap.read()
-    mask_resized = cv2.resize(mask, (frame.shape[1], frame.shape[0]))
-
     while True:
         _, frame = cap.read()
-        frame_region = cv2.bitwise_and(frame, mask_resized)
-        results = model.track(source=frame_region, persist=True)
+        results = model.track(source=frame, persist=True)
 
         for result in results:
             if result.boxes:
@@ -28,7 +22,7 @@ def main(video_path, scale):
                         track_id = int(box.id[0])
                         cls = int(box.cls[0])
                         class_name = model.names[cls]
-                        warna = pink if class_name == "Folding" else pink_reduce
+                        warna = pink if class_name == "person" else pink_reduce
                         cvzone.putTextRect(frame, f"{class_name} ID: {track_id}", (int(x1), int(y1 - 10)), scale=2, thickness=2, colorR=warna)
                     else:
                         print("Box ID is None, skipping")
@@ -46,5 +40,5 @@ def main(video_path, scale):
 
 if __name__ == "__main__":
     video_path = "rtsp://admin:oracle2015@192.168.100.65:554/Streaming/Channels/1"
-    # video_path = ".runs/videos/folding/01.mp4"
+    video_path = ".runs/videos/folding/01.mp4"
     main(video_path, scale=0.75)
