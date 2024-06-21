2a870b7ac61bb994fdf45a325b8eaa26151a5426
Troppo99 <troppolungo99@gmail.com>
2024-06-21 15:23:47 +0700

    CP2:

--- src/bsml4.py
+++ src/bsml4.py
@@ -10,31 +10,26 @@ def main(video_path, scale):
     pink = (255, 0, 255)
     pink_reduce = (255, 200, 255)
 
+    # Read and resize mask once outside the loop
+    mask = cv2.imread(".runs/images/mask4.png")
+    _, frame = cap.read()
+    mask_resized = cv2.resize(mask, (frame.shape[1], frame.shape[0]))
+
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        mask = cv2.imread(".runs/images/mask4.png")
-        if mask is None:
-            raise ValueError("Mask image not found")
-        mask_resized = cv2.resize(mask, (frame.shape[1], frame.shape[0]))
+        _, frame = cap.read()
         frame_region = cv2.bitwise_and(frame, mask_resized)
-
         results = model.track(source=frame_region, persist=True)
 
         for result in results:
             if result.boxes:
                 for box in result.boxes:
                     if box.id is not None and box.id[0] is not None:
-                        x1, y1, x2, y2 = box.xyxy[0]  # Convert to CPU and then to NumPy
-                        track_id = int(box.id[0])  # Get the track ID
-                        cls = int(box.cls[0])  # Get the class ID
-                        class_name = model.names[cls]  # Get the class name
-
-                        # Draw bounding box
+                        x1, y1, x2, y2 = box.xyxy[0]
+                        track_id = int(box.id[0])
+                        cls = int(box.cls[0])
+                        class_name = model.names[cls]
                         warna = pink if class_name == "Folding" else pink_reduce
-                        cvzone.putTextRect(frame, f"{class_name} ID: {track_id}", (int(x1), int(y1 - 10)), scale=1, thickness=1, colorR=warna)
+                        cvzone.putTextRect(frame, f"{class_name} ID: {track_id}", (int(x1), int(y1 - 10)), scale=2, thickness=2, colorR=warna)
                     else:
                         print("Box ID is None, skipping")
 
