aaa25b9ff5d33958f7fe668c571eefafb2d578f3
Troppo99 <troppolungo99@gmail.com>
2024-06-27 15:20:01 +0700

    mulai dan hapus OL dan SAVER dulu

--- src/exp.py
+++ src/exp.py
@@ -1,142 +1,216 @@
-from ultralytics import YOLO
+import torch
 import cv2
-import cvzone
+from ultralytics import YOLO
 import math
-import sort
-import numpy as np
+import cvzone
+from datetime import timedelta
 
-scaleof = 1.5
-newDim = (int(1280 * scaleof), int(720 * scaleof))
-cap = cv2.VideoCapture("D:/MY_FILES/Videos/cars.mp4")
-model = YOLO(".runs/weights/yolov8l.pt")
-classNames = [
-    "person",
-    "bicycle",
-    "car",
-    "motorbike",
-    "aeroplane",
-    "bus",
-    "train",
-    "truck",
-    "boat",
-    "traffic light",
-    "fire hydrant",
-    "stop sign",
-    "parking meter",
-    "bench",
-    "bird",
-    "cat",
-    "dog",
-    "horse",
-    "sheep",
-    "cow",
-    "elephant",
-    "bear",
-    "zebra",
-    "giraffe",
-    "backpack",
-    "umbrella",
-    "handbag",
-    "tie",
-    "suitcase",
-    "frisbee",
-    "skis",
-    "snowboard",
-    "sports ball",
-    "kite",
-    "baseball bat",
-    "baseball glove",
-    "skateboard",
-    "surfboard",
-    "tennis racket",
-    "bottle",
-    "wine glass",
-    "cup",
-    "fork",
-    "knife",
-    "spoon",
-    "bowl",
-    "banana",
-    "apple",
-    "sandwich",
-    "orange",
-    "broccoli",
-    "carrot",
-    "hot dog",
-    "pizza",
-    "donut",
-    "cake",
-    "chair",
-    "sofa",
-    "pottedplant",
-    "bed",
-    "diningtable",
-    "toilet",
-    "tvmonitor",
-    "laptop",
-    "mouse",
-    "remote",
-    "keyboard",
-    "cell phone",
-    "microwave",
-    "oven",
-    "toaster",
-    "sink",
-    "refrigerator",
-    "book",
-    "clock",
-    "vase",
-    "scissors",
-    "teddy bear",
-    "hair drier",
-    "toothbrush",
-]
-mask = cv2.imread("D:/MY_FILES/Images/mask.png")
-tracker = sort.Sort(max_age=20, min_hits=3, iou_threshold=0.3)
-limits = [350, 297, 800, 297]
-totalCount = []
-while True:
-    succes, img = cap.read()
-    imgRegion = cv2.bitwise_and(img, mask)
-    imgGraphics = cv2.imread("D:/MY_FILES/Images/graphics.png", cv2.IMREAD_UNCHANGED)
-    img = cvzone.overlayPNG(img, imgGraphics, (0, 0))
-    results = model(imgRegion, stream=True)
-    detections = np.empty((0, 5))
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            w, h = x2 - x1, y2 - y1
-            conf = math.ceil((box.conf[0] * 100)) / 100
-            cls = int(box.cls[0])
-            currentClass = classNames[cls]
-            if currentClass == "car" or currentClass == "bus" or currentClass == "truck" or currentClass == "motorbike" and conf > 0.5:
-                currentArray = np.array([x1, y1, x2, y2, conf])
-                detections = np.vstack((detections, currentArray))
-    resultTracker = tracker.update(detections)
-    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
-    for result in resultTracker:
-        x1, y1, x2, y2, id = result
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        print(result)
-        w, h = x2 - x1, y2 - y1
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, t=3, rt=5, colorR=(255, 0, 0))
-        cvzone.putTextRect(
-            img,
-            f"ID: {int(id)}",
-            (max(0, x1), max(35, y1)),
-            scale=1.5,
-            thickness=2,
-            offset=3,
-        )
-        cx, cy = x1 + w // 2, y1 + h // 2
-        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-        if limits[0] < cx < limits[2] and limits[1] - 20 < cy < limits[3] + 20:
-            if totalCount.count(id) == 0:
-                totalCount.append(id)
-                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
-    cv2.putText(img, str(len(totalCount)), (240, 100), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 8)
-    img = cv2.resize(img, newDim)
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+class AICCTV:
+    def __init__(self, video_path, mask_path, emp_model_path, act_model_path, emp_classes, act_classes):
+        self.cap = cv2.VideoCapture(video_path)
+        self.mask = cv2.imread(mask_path)
+        self.model_emp = YOLO(emp_model_path)
+        self.model_act = YOLO(act_model_path)
+        self.class_emp = emp_classes
+        self.class_act = act_classes
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Using device: {self.device}")
+
+    def process_frame(self, frame, mask):
+
+        frame_region = cv2.bitwise_and(frame, mask)
+
+        results_emp = self.model_emp(source=frame_region, stream=True)
+        frame, emp_boxes_info = self.process_results(frame, results_emp, self.class_emp, (0, 255, 0))
+
+        act_boxes_info = []
+        if emp_boxes_info:
+            results_act = self.model_act(source=frame_region, stream=True)
+            frame, act_boxes_info = self.process_results(frame, results_act, self.class_act, (255, 0, 0))
+
+        return frame, emp_boxes_info, act_boxes_info
+
+    def process_results(self, frame, results, classes, color):
+        boxes_info = []
+        for result in results:
+            boxes = result.boxes.cpu().numpy()
+            for box in boxes:
+                x1, y1, x2, y2 = self.get_coordinates(box)
+                conf = self.get_confidence(box)
+                class_id = classes[int(box.cls[0])]
+                boxes_info.append((x1, y1, x2, y2, class_id, conf, color))
+        return frame, boxes_info
+
+    @staticmethod
+    def get_coordinates(box):
+        x1, y1, x2, y2 = box.xyxy[0]
+        return int(x1), int(y1), int(x2), int(y2)
+
+    @staticmethod
+    def get_confidence(box):
+        return math.ceil(box.conf[0] * 100) / 100
+
+    @staticmethod
+    def resize_frame(frame, scale=0.4):
+        height = int(frame.shape[0] * scale)
+        width = int(frame.shape[1] * scale)
+        return cv2.resize(frame, (width, height))
+
+    @staticmethod
+    def draw_box(frame, x1, y1, x2, y2, text, color, thickness=2, font_scale=2, font_thickness=2):
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, thickness)
+        cvzone.putTextRect(frame, text, (max(0, x1), max(35, y1)), scale=font_scale, thickness=font_thickness)
+
+    @staticmethod
+    def is_overlapping(box1, box2):
+        x1, y1, x2, y2 = box1
+        ax1, ay1, ax2, ay2 = box2
+        if x1 < ax2 and x2 > ax1 and y1 < ay2 and y2 > ay1:
+            return True
+        return False
+
+
+class REPORT:
+    def __init__(self, emp_classes):
+        self.data = {}
+        self.emp_classes = emp_classes
+
+    def update_data_table(self, emp_class, act_class, frame_duration):
+        if emp_class not in self.data:
+            self.data[emp_class] = {
+                "folding_time": 0,
+                "idle_time": 0,
+                "offsite_time": 0,
+            }
+        if act_class in self.data[emp_class]:
+            self.data[emp_class][act_class] += frame_duration
+        else:
+            self.data[emp_class]["idle_time"] += frame_duration
+
+    def calculate_percentages(self):
+        percentages = {}
+        for emp_class in self.data:
+            total_emp_time = sum(self.data[emp_class].values())
+            percentages[emp_class] = {}
+            for key in self.data[emp_class]:
+                percentage_key = f"%{key[0]}"
+                if total_emp_time > 0:
+                    percentages[emp_class][percentage_key] = (self.data[emp_class][key] / total_emp_time) * 100
+                else:
+                    percentages[emp_class][percentage_key] = 0
+        return percentages
+
+    def draw_table(self, frame, percentages, row_height=42, x_move=2000, y_move=600, pink_color=(255, 0, 255), dpink_color=(145, 0, 145), scale_text=3):
+        def format_time(seconds):
+            return str(timedelta(seconds=int(seconds)))
+
+        cv2.putText(frame, f"Report Table", (-140 + x_move, 540 + y_move), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1.8, (20, 200, 20), 2, cv2.LINE_AA)
+        headers = ["Employee", "Folding", "Idle", "Offsite"]
+
+        cv2.putText(frame, headers[0], (-160 + x_move, 595 + y_move), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(frame, headers[1], (90 + x_move, 595 + y_move), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(frame, headers[2], (430 + x_move, 595 + y_move), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(frame, headers[3], (770 + x_move, 595 + y_move), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 255, 255), 3, cv2.LINE_AA)
+
+        for row_idx, (emp_class, times) in enumerate(self.data.items(), start=1):
+            color_rect = pink_color if (row_idx % 2) == 0 else dpink_color
+            y_position = 610 + row_idx * row_height
+
+            cvzone.putTextRect(frame, emp_class, (-160 + x_move, y_position + y_move), scale=scale_text, thickness=2, offset=5, colorR=color_rect)
+
+            cvzone.putTextRect(frame, format_time(times["folding_time"]), (90 + x_move, y_position + y_move), scale=scale_text, thickness=2, offset=5, colorR=color_rect)
+            cvzone.putTextRect(frame, f"{(percentages[emp_class]['%f']):.0f}%", (285 + x_move, y_position + y_move), scale=scale_text, thickness=2, offset=5, colorR=color_rect)
+
+            cvzone.putTextRect(frame, format_time(times["idle_time"]), (430 + x_move, y_position + y_move), scale=scale_text, thickness=2, offset=5, colorR=color_rect)
+            cvzone.putTextRect(frame, f"{percentages[emp_class]['%i']:.0f}%", (625 + x_move, y_position + y_move), scale=scale_text, thickness=2, offset=5, colorR=color_rect)
+
+            cvzone.putTextRect(frame, format_time(times["offsite_time"]), (770 + x_move, y_position + y_move), scale=scale_text, thickness=2, offset=5, colorR=color_rect)
+            cvzone.putTextRect(frame, f"{percentages[emp_class]['%o']:.0f}%", (965 + x_move, y_position + y_move), scale=scale_text, thickness=2, offset=5, colorR=color_rect)
+
+
+class VideoSaver:
+    def __init__(self, output_path, frame_width, frame_height, fps=20.0):
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        self.out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+
+    def write_frame(self, frame):
+        self.out.write(frame)
+
+    def release(self):
+        self.out.release()
+
+
+video_path = "D:/AI_CCTV/.runs/videos/0624.mp4"
+# video_path = "rtsp://admin:oracle2015@192.168.100.6:554/Streaming/Channels/1"
+mask_path = ".runs/images/mask7.png"
+emp_model_path = ".runs/detect/.arc/two_women/weights/best.pt"
+act_model_path = ".runs/detect/.arc/emp_gm1_rev/weights/best.pt"
+emp_classes = ["Siti Umi", "Nina"]
+act_classes = ["Idle", "Folding"]
+
+ai_cctv = AICCTV(video_path, mask_path, emp_model_path, act_model_path, emp_classes, act_classes)
+report = REPORT(emp_classes)
+frame_rate = ai_cctv.cap.get(cv2.CAP_PROP_FPS)
+
+""" #######-> Start of Video Saver 1 <-####### """
+# ret, frame = ai_cctv.cap.read()
+# video_saver = VideoSaver(".runs/videos/writer/output_video.mp4", frame.shape[1], frame.shape[0], frame_rate)
+""" --------> End of Video Saver 1 <-------- """
+
+""" #######-> Start of overlay 2 <-####### """
+# table_bg = cv2.imread(".runs/images/OL1.png", cv2.IMREAD_UNCHANGED)
+# new_width = 1350
+# aspect_ratio = table_bg.shape[1] / table_bg.shape[0]
+# new_height = int(new_width / aspect_ratio)
+# table_bg = cv2.resize(table_bg, (new_width, new_height))
+""" --------> End of overlay 2 <-------- """
+
+while ai_cctv.cap.isOpened():
+    _, frame = ai_cctv.cap.read()
+    mask_resized = cv2.resize(ai_cctv.mask, (frame.shape[1], frame.shape[0]))
+
+    frame, emp_boxes_info, act_boxes_info = ai_cctv.process_frame(frame, mask_resized)
+    frame_duration = 1 / frame_rate
+
+    for x1, y1, x2, y2, emp_class, _, _ in emp_boxes_info:
+        act_detected = False
+        for ax1, ay1, ax2, ay2, act_class, _, _ in act_boxes_info:
+            if ai_cctv.is_overlapping((x1, y1, x2, y2), (ax1, ay1, ax2, ay2)):
+                act_detected = True
+                report.update_data_table(emp_class, act_class.lower() + "_time", frame_duration)
+                text = f"{emp_class} is {act_class}"
+                ai_cctv.draw_box(frame, x1, y1, x2, y2, text, (0, 255, 0))
+                break
+        if not act_detected:
+            report.update_data_table(emp_class, "idle_time", frame_duration)
+            text = f"{emp_class} is idle"
+            ai_cctv.draw_box(frame, x1, y1, x2, y2, text, (255, 255, 0))
+
+    detected_employees = [emp_class for _, _, _, _, emp_class, _, _ in emp_boxes_info]
+    for emp_class in emp_classes:
+        if emp_class not in detected_employees:
+            report.update_data_table(emp_class, "offsite_time", frame_duration)
+
+    """ #######-> Start of overlay 2 <-####### """
+    # frame = cvzone.overlayPNG(frame, table_bg, (1800, 1055))
+    # cv2.imshow("Video with Overlay", frame_with_overlay)
+    """ --------> End of overlay 2 <-------- """
+    percentages = report.calculate_percentages()
+    report.draw_table(frame, percentages)
+
+    """ * * *---> Start of Video Saver 1 <---* * * """
+    # video_saver.write_frame(frame)
+    """ --------> End of Video Saver 1 <-------- """
+
+    frame = ai_cctv.resize_frame(frame)
+    cv2.imshow("AI on Folding Area", frame)
+    if cv2.waitKey(1) & 0xFF == ord("n"):
+        break
+
+""" * * *---> Start of Video Saver 1 <---* * * """
+# video_saver.release()
+""" --------> End of Video Saver 1 <-------- """
+
+ai_cctv.cap.release()
+cv2.destroyAllWindows()
