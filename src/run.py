65f43c99a3b9d740ebeab049f6ceefa2c6f22875
Troppo99 <troppolungo99@gmail.com>
2024-06-28 13:19:13 +0700

    mencoba menjadi 3 file

--- src/run.py
+++ src/run.py
@@ -1,10 +1,10 @@
-import bsml4
+import main
 
-bsml4.main(
-    mask_path=".runs/images/mask7.png",
+main.main(
     emp_model_path=".runs/detect/.arc/two_women/weights/best.pt",
-    act_model_path=".runs/weights/fold.pt",
+    act_model_path=".runs/detect/fold-m/weights/best.pt",
     emp_classes=["Siti Umi", "Nina"],
-    act_classes=["Folding", "Idle"],
+    act_classes=["Folding"],
     video_path="D:/AI_CCTV/.runs/videos/00000000302000000.mp4",
+    # mask_path=".runs/images/mask7.png",
 )
--- train/train.py
+++ train/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     model = YOLO(".runs/weights/yolov8m.pt")
-    model.train(data="D:/MY_FILES/Datasets/employee_folding_area.v4i.yolov8/data.yaml", epochs=50, imgsz=736, project=".runs/detect", name="person-m-v2")
+    model.train(data="D:/MY_FILES/Datasets/act_gm1_revised 1/data.yaml", epochs=50, imgsz=736, project=".runs/detect", name="fold-m")
