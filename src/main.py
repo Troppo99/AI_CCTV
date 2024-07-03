4242ffd7ba932468602669f8b1161cd1dd06546b
Troppo99 <troppolungo99@gmail.com>
2024-07-03 10:44:20 +0700

    r4esize dan git .arc

--- src/main.py
+++ src/main.py
@@ -40,9 +40,10 @@ def main(
             timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
             print(f"- - -\nframe is None, Buddy! When it was {timestamp}")
             print(f"Program is running for {current_time-start_time:.0f}!\n- - -")
-        mask_resized = cv2.resize(mask, (frame.shape[1], frame.shape[0])) if mask_path is not None else None
+        resized_frame = ai_cctv.resize_frame(frame)
+        mask_resized = cv2.resize(mask, (resized_frame.shape[1], resized_frame.shape[0])) if mask is not None else None
         frame_duration = 1 / frame_rate
-        frame, emp_boxes_info, act_boxes_info = ai_cctv.process_frame(frame, mask_resized)
+        frame, emp_boxes_info, act_boxes_info = ai_cctv.process_frame(resized_frame, mask_resized)
         for x1, y1, x2, y2, emp_class, _, emp_color in emp_boxes_info:
             act_detected = False
             for ax1, ay1, ax2, ay2, act_class, _, act_color in act_boxes_info:
