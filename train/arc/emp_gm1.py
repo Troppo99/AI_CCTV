c78942829674594e30fa86e6c5bee6f74f387858
Troppo99 <troppolungo99@gmail.com>
2024-07-03 10:45:22 +0700

    update arc

--- train/arc/emp_gm1.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO(".runs/weights/yolov8l.pt")
-model.train(data="", epochs=50, imgsz=640, project=".runs/detect", name="")
--- train/arc/two_women.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-if __name__ == "__main__":
-    model = YOLO(".runs/weights/yolov8l.pt")
-    model.train(data="D:/MY_FILES/Datasets/two_women/data.yaml", epochs=50, imgsz=640, project=".runs/detect", name="two_women")
