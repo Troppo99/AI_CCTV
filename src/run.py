c5fccaa2aa5cef0875bffbdc78adb8644e7a4d4a
Troppo99 <troppolungo99@gmail.com>
2024-07-05 16:27:53 +0700

    update

--- src/run.py
+++ src/run.py
@@ -4,9 +4,9 @@ main(
     mask_path=".runs/images/mask8.png",
     anto_time=300,
     interval_send=10,
-    server="Waskita",
+    # server="Waskita",
     # show = True,
-    # server="Nana",
+    server="Nana",
     # save=True,
     # video_path="D:/AI_CCTV/.runs/videos/00000000302000000.mp4",
 )
