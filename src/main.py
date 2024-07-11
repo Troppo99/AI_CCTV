9256391d3840b9bb459b8124084888fad485f951
Troppo99 <troppolungo99@gmail.com>
2024-07-11 15:11:05 +0700

    update

--- src/main.py
+++ src/main.py
@@ -19,17 +19,17 @@ def main(
     anto_time=3,
 ):
     start_time = time.time()
-    ai_cctv = AICCTV(emp_model_path, act_model_path, emp_classes, act_classes, video_path, server)
+    aicctv = AICCTV(emp_model_path, act_model_path, emp_classes, act_classes, video_path, server)
     report = REPORT(emp_classes, anto_time, interval_send)
-    frame_rate = ai_cctv.cap.get(cv2.CAP_PROP_FPS)
+    frame_rate = aicctv.cap.get(cv2.CAP_PROP_FPS)
     frame_queue = queue.Queue()
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.submit(ai_cctv.capture_frame, frame_queue)
+        executor.submit(aicctv.capture_frame, frame_queue)
         if server:
             send = True
             host, user, password, database, port = report.where_sql_server(server)
         mask = cv2.imread(mask_path) if mask_path is not None else None
-        while ai_cctv.cap.isOpened():
+        while aicctv.cap.isOpened():
             if not frame_queue.empty():
                 frame = frame_queue.get()
                 if frame is None:
@@ -39,20 +39,20 @@ def main(
                     print(f"Program is running for {current_time-start_time:.0f}!\n- - -")
                 mask_resized = cv2.resize(mask, (frame.shape[1], frame.shape[0])) if mask is not None else None
                 frame_duration = 1 / frame_rate
-                frame, emp_boxes_info, act_boxes_info = ai_cctv.process_frame(frame, mask_resized)
+                frame, emp_boxes_info, act_boxes_info = aicctv.process_frame(frame, mask_resized)
                 for x1, y1, x2, y2, emp_class, _, emp_color in emp_boxes_info:
                     act_detected = False
                     for ax1, ay1, ax2, ay2, act_class, _, act_color in act_boxes_info:
-                        if ai_cctv.is_overlapping((x1, y1, x2, y2), (ax1, ay1, ax2, ay2)):
+                        if aicctv.is_overlapping((x1, y1, x2, y2), (ax1, ay1, ax2, ay2)):
                             act_detected = True
                             report.update_data_table(emp_class, "working_time", frame_duration)
                             text = f"{emp_class} is {act_class}"
-                            ai_cctv.draw_box(frame, x1, y1, x2, y2, text, act_color)
+                            aicctv.draw_box(frame, x1, y1, x2, y2, text, act_color)
                             break
                     if not act_detected:
                         report.update_data_table(emp_class, "idle_time", frame_duration)
                         text = f"{emp_class} is idle"
-                        ai_cctv.draw_box(frame, x1, y1, x2, y2, text, emp_color)
+                        aicctv.draw_box(frame, x1, y1, x2, y2, text, emp_color)
                 detected_employees = [emp_class for _, _, _, _, emp_class, _, _ in emp_boxes_info]
                 for emp_class in emp_classes:
                     if emp_class not in detected_employees:
@@ -60,7 +60,7 @@ def main(
                 if show:
                     percentages = report.calculate_percentages()
                     report.draw_table(frame, percentages)
-                frame = ai_cctv.resize_frame(frame)
+                frame = aicctv.resize_frame(frame)
                 if show:
                     mask_info = mask_path.split("/")[-1] if mask_path else mask_path
                     data_info = f"Sending to {host}" if send else "Not sending"
@@ -79,7 +79,7 @@ def main(
                 if cv2.waitKey(1) & 0xFF == ord("n"):
                     break
 
-        ai_cctv.cap.release()
+        aicctv.cap.release()
         cv2.destroyAllWindows()
 
 
