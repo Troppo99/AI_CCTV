b9a20a803abd2fad87ee0a2b79571ad0b733781a
Troppo99 <troppolungo99@gmail.com>
2024-07-16 11:26:08 +0700

    menambahkan apply_nms

--- src/bsml4.py
+++ src/bsml4.py
@@ -48,6 +48,38 @@ class AICCTV:
                 boxes_info.append((x1, y1, x2, y2, class_id, conf, color))
         return frame, boxes_info
 
+    @staticmethod
+    def apply_nms(boxes, confidences, iou_threshold=0.4):
+        if len(boxes) == 0:
+            return []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+        order = confidences.argsort()[::-1]
+
+        keep = []
+        while order.size > 0:
+            i = order[0]
+            keep.append(i)
+            xx1 = np.maximum(x1[i], x1[order[1:]])
+            yy1 = np.maximum(y1[i], y1[order[1:]])
+            xx2 = np.minimum(x2[i], x2[order[1:]])
+            yy2 = np.minimum(y2[i], y2[order[1:]])
+
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+            inter = w * h
+            ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+            inds = np.where(ovr <= iou_threshold)[0]
+            order = order[inds + 1]
+
+        return keep
+
     def capture_frame(self, frame_queue):
         while True:
             if not self.cap.isOpened():
